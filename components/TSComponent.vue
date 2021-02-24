bb7742807d051e778a424ebe795dbdbfa5575df7
Cody Jackson <sxenog@gmail.com>
2021-02-23 18:33:22 -0700

    Got typescript component included and running

---
+++ components/TSComponent.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+export interface Value {
+    label: String;
+}
+
+@Component
+class TSComponent extends Vue {
+    @Prop(Object) value: Value;
+}
+
+export default TSComponent;
+
+</script>
+
+<template>
+  <div>{{ value.label }}</div>
+</template>
---
+++ pages/ts/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import TSComponent from '@/components/TSComponent.vue';
+
+const Component = defineComponent({ components: { TSComponent } });
+
+export default Component;
+</script>
+
+<template>
+  <TSComponent :value="{label: 'interfaces'}" />
+</template>
