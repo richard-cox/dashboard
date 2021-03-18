f97029ce9617e8912904d09f9b0a285322a9be6e
Richard Cox <richard.cox@suse.com>
2021-03-18 15:24:31 +0000

    asyng fetch + $fetchState

--- components/TSComponentRichard.vue
+++ components/TSComponentRichard.vue
@@ -1,37 +1,73 @@
+
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
-import { COUNT, NAMESPACE } from "~/config/types";
-import { ProxiedResource } from "~/plugins/steve/resource.types";
-import { IDashboardNamespace, RancherNamespace } from "~/models/namespace";
+// eslint-disable no-console
+import { Component, Vue, Prop, Watch } from 'nuxt-property-decorator';
+import { COUNT, NAMESPACE } from '~/config/types';
+import { ProxiedResource } from '~/plugins/steve/resource.types';
+import { IDashboardNamespace, RancherNamespace } from '~/models/namespace';
 
 interface Value {
   label: String;
 }
 
-@Component
+
+// Component.registerHooks(['fetch']);
+
+// eslint-disable-next-line no-use-before-define
+@Component<TSComponentRichard>({
+  // Not called
+  async fetch(ctx) {
+    console.error('fetch', this.$isServer);
+    // testString auto completes
+    console.error('this.testString: ', this.testString);
+    // namespace auto completes
+    await this.$store.dispatch('cluster/find', { type: NAMESPACE, id: 'ds4-4-1-charts' });
+  },
+  // asyndDate is only for page components
+  // async asyncData(ctx) {
+  //   return {
+  //     test:      true,
+  //   };
+  // },
+})
 class TSComponentRichard extends Vue {
-  @Prop(Object) value: Value;
+  @Prop({ default: { label: '' } }) value: Value;
 
-  counts: any;
-  namespace: any;
-  namespaceTyped: ProxiedResource<IDashboardNamespace, RancherNamespace>;
+  get computedValue() {
+    return `computed-${ this.value }`;
+  }
 
-  constructor() {
-    super();
+  get computedNamespace() {
+    return this.namespace || {}
+  }
 
+  // @Watch('namespace')
+  // onPropertyChanged() {
+  //   // value: string, oldValue: string
+  //   console.log('namespace changed');
+  // }
+
+  private counts: any;
+  private namespace: any;
+  private namespaceTyped: ProxiedResource<IDashboardNamespace, RancherNamespace>;
+
+  private readonly testString = 'this is a string';
+
+  created() {
+    console.error('this.$fetchState', this.$fetchState)
     this.counts = this.$store.getters[`cluster/all`](COUNT)[0].counts[
       NAMESPACE
     ];
-    this.namespace = this.$store.getters["cluster/byId"](
+    this.namespace = this.$store.getters['cluster/byId'](
       NAMESPACE,
-      "ds4-4-1-charts"
+      'ds4-4-1-charts'
     );
     this.namespaceTyped = this.namespace as ProxiedResource<
       IDashboardNamespace,
       RancherNamespace
     >;
 
-const a = '';
+    const a = '';
   }
 }
 
@@ -39,6 +75,8 @@ export default TSComponentRichard;
 </script>
 
 <template>
+  <!-- $fetchState is undefined -->
+  <!-- <table v-if="!$fetchState.pending"> -->
   <table>
     <tr>
       <th>Source</th>
@@ -48,17 +86,17 @@ export default TSComponentRichard;
       <td>this.$store.getters[`cluster/all`](COUNT)[0].counts[NAMESPACE]</td>
       <td>{{ counts }}</td>
     </tr>
-    <tr>
+    <tr v-if="namespace">
       <td>this.$store.getters['cluster/byId'](NAMESPACE, 'ds4-4-1-charts')</td>
       <td>{{ namespace }}</td>
     </tr>
-    <tr>
+     <tr v-if="computedNamespace">
       <td>namespace.nameDisplay</td>
-      <td>{{ namespace.nameDisplay }}</td>
-    </tr>
+      <td>{{ computedNamespace.nameDisplay }}</td>
+    </tr> 
     <tr>
       <td>namespace.projectNameSort</td>
-      <td>{{ namespace.projectNameSort }}</td>
+      <td>{{ namespace.istioInstalled }}</td>
     </tr>
     <tr>
       <td>namespaceTyped.nameDisplay</td>
@@ -66,8 +104,8 @@ export default TSComponentRichard;
     </tr>
     <tr>
       <td>namespaceTyped.projectNameSort</td>
-      <td>{{ namespaceTyped.projectNameSort }}</td>
-    </tr>
+      <td>{{ namespaceTyped.istioInstalled }}</td>
+    </tr> 
   </table>
 </template>
 
