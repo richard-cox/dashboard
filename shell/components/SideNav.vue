2f3b95c96b732d46ab62d0b05dafbb68d149e3a5
Richard Cox <richard.cox@suse.com>
2023-10-31 15:51:23 +0000

    imports

--- shell/components/SideNav.vue
+++ shell/components/SideNav.vue
@@ -13,15 +13,12 @@ import {
 import { sortBy } from '@shell/utils/sort';
 import { ucFirst } from '@shell/utils/string';
 
-import {
-  HCI, CATALOG, UI, SCHEMA, COUNT
-} from '@shell/config/types';
+import { HCI, CATALOG, UI, SCHEMA } from '@shell/config/types';
 import { HARVESTER_NAME as HARVESTER } from '@shell/config/features';
 import { NAME as EXPLORER } from '@shell/config/product/explorer';
 import { TYPE_MODES } from '@shell/store/type-map';
 import { NAME as NAVLINKS } from '@shell/config/product/navlinks';
 import Group from '@shell/components/nav/Group';
-import { diff } from '@shell/utils/object';
 
 export default {
   name:       'SideNav',
