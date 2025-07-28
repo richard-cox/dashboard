50c94e71822c044778cbd4f7fdb39d7730783a09
Richard Cox <ricox@suse.com>
2025-07-28 09:22:09 +0100

    Ensure new system based secrets don't appear when `User only` namespace filter is enabled

--- shell/config/system-namespaces.js
+++ shell/config/system-namespaces.js
@@ -1,9 +1,12 @@
-// These are used to determine what is considered a User vs System namespace in the namespace picker,
-// along with the `isSystem` property of the project associated with the namespace, if in Rancher mode.
+/**
+ * These are used to determine what is considered a User vs System namespace in the namespace picker,
+ * along with the `isSystem` property of the project associated with the namespace, if in Rancher mode.
+ */
 export default [
   'cattle-dashboards',
   'cattle-global-data',
   'cattle-system',
+  // 'cattle-local-user-passwords',
   'gatekeeper-system',
   'ingress-nginx',
   'kube-node-lease',
